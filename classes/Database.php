453651bae8c6a0eae0bfac0e33563289a5a4434c
Alexis Thierry <alexist103@gmail.com>
2023-05-16 15:21:17 +0200

    Feature: adding Database

--- classes/Database.php
+++ classes/Database.php
@@ -27,6 +27,12 @@ if ( ! class_exists( 'Database' ) ) {
 			$conn->query( 'SET NAMES utf8' );
 		}
 
+		/**
+		 * @param $email
+		 * @param $password
+		 *
+		 * @return bool
+		 */
 		public static function get_questions() {
 			global $conn;
 			$sql = 'SELECT * FROM question';
@@ -42,4 +48,55 @@ if ( ! class_exists( 'Database' ) ) {
 				);
 				array_push( $questions, $question );
 			}
+			return $questions;
 		}
+
+		/**
+		 * @param $email
+		 * @param $password
+		 *
+		 * @return bool
+		 */
+		public static function get_question_by_id( $id ) {
+			global $conn;
+			$sql = 'SELECT * FROM question WHERE id = ' . $id;
+			$res = mysqli_query( $conn, $sql );
+			$question = array();
+			foreach( $res as $row ) {
+				$question = array(
+					'id' => $row['id'],
+					'title' => $row['title'],
+					'content' => $row['content'],
+					'creation_date' => $row['creation_date'],
+					'number_like' => $row['number_like']
+				);
+			}
+			return $question;
+		}
+
+		/**
+		 * @param $email
+		 * @param $password
+		 *
+		 * @return bool
+		 */
+		public static function get_user_by_id( $id ) {
+			global $conn;
+			$sql = 'SELECT * FROM user WHERE id = ' . $id;
+			$res = mysqli_query( $conn, $sql );
+			$user = array();
+			foreach( $res as $row ) {
+				$user = array(
+					'id' => $row['id'],
+					'user_name' => $row['user_name'],
+					'first_name' => $row['first_name'],
+					'last_name' => $row['last_name'],
+					'is_admin' => $row['is_admin']
+				);
+			}
+			return $user;
+		}
+
+		
+	}
+}

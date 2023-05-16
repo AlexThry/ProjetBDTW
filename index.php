eabc55ab11ffc9b827c74173283f06b00ca9e6cf
carlyyne <carlyne.barrachin@gmail.com>
2023-05-16 15:33:14 +0200

    FEATURE:text of the main page

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 		<div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
 			<div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
 			<div class="sm:max-w-lg">
-				<h1 class="font text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">Des milliers d'avis sur vos livres préférés</h1>
-				<p class="mt-4 text-xl text-gray-500 dark:text-gray-300">Découvrez de nouveaux horizons littéraires en partageant votre avis sur vos livres préférés dès maintenant sur notre site !</p>
+				<h1 class="font text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">Ne laissez aucune question sans réponse</h1>
+				<p class="mt-4 text-xl text-gray-500 dark:text-gray-300">Trouvez des solutions et approfondissez vos connaissances en posant toutes vos questions sur le cours sur notre site interactif et bénéficiez des réponses de nos experts!</p>
 			</div>
 			<div>
 				<div class="mt-10">

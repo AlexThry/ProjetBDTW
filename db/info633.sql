1bed597671eafe650b496a1b2a14c142084d5e45
loisblin <113303000+loisblin@users.noreply.github.com>
2023-05-21 18:06:47 +0200

    [FIX] : id_user in db

--- db/info633.sql
+++ db/info633.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : dim. 21 mai 2023 à 17:53
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Généré le : mer. 17 mai 2023 à 09:00
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -156,19 +156,19 @@ CREATE TABLE IF NOT EXISTS `question` (
 -- Déchargement des données de la table `question`
 --
 
-INSERT INTO `question` (`id`, `title`, `creation_date`, `number_likes`, `content`, `id_user`) VALUES
-(1, 'Connexion à la base de donnée en PHP', '2023-02-02 00:00:00', 0, 'J\'ai une erreur quand j\'essaye de connecter mon site à ma base de donnée mysql, j\'obtiens l\'error \"Vous n\'êtes pas qualifié pour parler de mon grand front\".\r\nVoici mon code :\r\n<?php\r\n$serveur = \"localhost\";\r\n$utilisateur = \"jeanlasticot\";\r\n$motdepasse = \"jesuisunasticot joyeux\";\r\n$base_de_donnees =\"aaaaaaugh\";\r\n\r\n$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $base_de_donnees);\r\n\r\nif (!$connexion) {\r\n    die(\"Erreur de connexion à la base de données : \" . mysqli_connect_error());\r\n}', 2),
-(2, 'Transformation d’une association de 1 à 1', '2023-01-29 00:00:00', 4, 'Bonjour, nous avons vu comment passer du schéma entités/associations au schéma relationnel pour des associations de 1 à plusieurs et de plusieurs à plusieurs. Mais pour une association de 1 à 1 comment cela se passe ? On ajoute n\'importe laquelle clé primaire d\'une entité dans la relation de l\'autre entité en tant que clé étrangère ?\r\n', 1),
-(3, 'Nommage des tables', '2023-01-30 00:00:00', 12, 'Est ce qu\'il existe des conventions de nommages des tables / attributs dans le milieu professionnel de la base de donnés ?', 5),
-(4, 'Table et relation', '2023-01-30 00:00:00', 1, 'Bonjour, je me demandais quelle est la différence entre une table et une relation ?', 3),
-(5, 'Nombre de relation', '2023-01-30 00:00:00', 5, 'est-il possible d\'avoir deux ou plusieurs relations entre deux mêmes entités ?', 6),
-(6, 'tuple et n-tuple', '2023-01-31 00:00:00', 11, 'Que signifie \"tuple\" et \"n-uplet\" d\'un modèle ?', 7),
-(7, 'Merise et modèle entité association', '2023-01-01 00:00:00', 0, 'Quelles est la différence entre le modèle E/A et le modèle Merise ?\r\n', 4),
-(8, 'Groupe by et Order by', '2023-02-14 00:00:00', 27, 'Quelle est la différence entre GROUP BY et ORDER BY ?', 1),
-(9, 'Join et select imbriqué', '2023-02-22 00:00:00', 5, 'Bonjour, \r\n\r\nJe me demandais dans quelle situation il est préférable d\'utiliser un SELECT imbriquée et dans quelle situation il est préférable d\'utiliser un JOIN ?\r\n\r\nCordialement,\r\n\r\nUgo TAFARO', 3),
-(10, 'Left et right join en SQL', '2023-02-22 00:00:00', 12, 'Bonjour, \r\n\r\nJe souhaiterais savoir à quoi servent les left et right join puisqu\'ils sélectionnent l\'intégralité d\'une seule table (comme un SELECT * FROM table).\r\n\r\nCordialement', 6),
-(11, 'Jointure et produit cartésien', '2023-02-22 00:00:00', 1, 'Bonjour,\r\n\r\nj\'aimerais connaître la différence entre une jointure et un produit cartésien\r\n\r\nmerci,', 5),
-(12, 'Usage des vues', '2023-02-27 00:00:00', 5, 'Quels sont les désavantages d\'utiliser une vue au lieu d\'une table ?', 2);
+INSERT INTO `question` (`id`, `title`, `creation_date`, `number_likes`, `content`) VALUES
+(1, 'Connexion à la base de donnée en PHP', '2023-02-02 00:00:00', 0, 'J\'ai une erreur quand j\'essaye de connecter mon site à ma base de donnée mysql, j\'obtiens l\'error \"Vous n\'êtes pas qualifié pour parler de mon grand front\".\r\nVoici mon code :\r\n<?php\r\n$serveur = \"localhost\";\r\n$utilisateur = \"jeanlasticot\";\r\n$motdepasse = \"jesuisunasticot joyeux\";\r\n$base_de_donnees =\"aaaaaaugh\";\r\n\r\n$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $base_de_donnees);\r\n\r\nif (!$connexion) {\r\n    die(\"Erreur de connexion à la base de données : \" . mysqli_connect_error());\r\n}'),
+(2, 'Transformation d’une association de 1 à 1', '2023-01-29 00:00:00', 4, 'Bonjour, nous avons vu comment passer du schéma entités/associations au schéma relationnel pour des associations de 1 à plusieurs et de plusieurs à plusieurs. Mais pour une association de 1 à 1 comment cela se passe ? On ajoute n\'importe laquelle clé primaire d\'une entité dans la relation de l\'autre entité en tant que clé étrangère ?\r\n'),
+(3, 'Nommage des tables', '2023-01-30 00:00:00', 12, 'Est ce qu\'il existe des conventions de nommages des tables / attributs dans le milieu professionnel de la base de donnés ?'),
+(4, 'Table et relation', '2023-01-30 00:00:00', 1, 'Bonjour, je me demandais quelle est la différence entre une table et une relation ?'),
+(5, 'Nombre de relation', '2023-01-30 00:00:00', 5, 'est-il possible d\'avoir deux ou plusieurs relations entre deux mêmes entités ?'),
+(6, 'tuple et n-tuple', '2023-01-31 00:00:00', 11, 'Que signifie \"tuple\" et \"n-uplet\" d\'un modèle ?'),
+(7, 'Merise et modèle entité association', '2023-01-01 00:00:00', 0, 'Quelles est la différence entre le modèle E/A et le modèle Merise ?\r\n'),
+(8, 'Groupe by et Order by', '2023-02-14 00:00:00', 27, 'Quelle est la différence entre GROUP BY et ORDER BY ?'),
+(9, 'Join et select imbriqué', '2023-02-22 00:00:00', 5, 'Bonjour, \r\n\r\nJe me demandais dans quelle situation il est préférable d\'utiliser un SELECT imbriquée et dans quelle situation il est préférable d\'utiliser un JOIN ?\r\n\r\nCordialement,\r\n\r\nUgo TAFARO'),
+(10, 'Left et right join en SQL', '2023-02-22 00:00:00', 12, 'Bonjour, \r\n\r\nJe souhaiterais savoir à quoi servent les left et right join puisqu\'ils sélectionnent l\'intégralité d\'une seule table (comme un SELECT * FROM table).\r\n\r\nCordialement'),
+(11, 'Jointure et produit cartésien', '2023-02-22 00:00:00', 1, 'Bonjour,\r\n\r\nj\'aimerais connaître la différence entre une jointure et un produit cartésien\r\n\r\nmerci,'),
+(12, 'Usage des vues', '2023-02-27 00:00:00', 5, 'Quels sont les désavantages d\'utiliser une vue au lieu d\'une table ?');
 
 -- --------------------------------------------------------
 

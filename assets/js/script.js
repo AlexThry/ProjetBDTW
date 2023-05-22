a65d027485f8091eaa4262968129441393e7ca96
Arnaud Pfundstein <arnaudpfundstein.pro@gmail.com>
2023-05-22 10:54:50 +0200

    Clean&fix: fix issue in the handleRenrererClick js callback, clean and add js assets and add instruction to implement the new-question-form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,73 @@ function initSearchDropdown() {
   });
 }
 
+function initCategoriesSelector() {
+  const catDropdown = document.getElementById("categories-dropdown");
+  const catRenderer = document.getElementById("categories-renderer");
+
+  if (!catRenderer || !catDropdown) return;
+
+  function updateRenderer() {
+    catRenderer.style.display = "none";
+    const catLis = Array.from(
+      catDropdown.querySelectorAll('li input[type="checkbox"]')
+    )
+      .map((el) =>
+        el.checked ? { id: el.value, label: el.dataset.catLabel } : null
+      )
+      .filter((el) => el !== null);
+
+    let content = "";
+    for (const { label, id } of catLis) {
+      content +=
+        '<span class="cat-container inline-flex items-center px-2 py-1 mr-2 text-sm font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300" data-cat-id="' +
+        id +
+        '">\
+			' +
+        label +
+        '\
+			<button type="button" class="inline-flex items-center p-0.5 ml-2 text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300" aria-label="Remove">\
+				<svg aria-hidden="true" class="w-3.5 h-3.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>\
+				<span class="sr-only">Retirer la catégorie</span>\
+			</button>\
+		</span>';
+    }
+
+    if (content) {
+      catRenderer.style.display = "flex";
+    }
+
+    catRenderer.innerHTML = content;
+  }
+
+  updateRenderer();
+  catDropdown.addEventListener("click", updateRenderer);
+
+  function handleRendererClick(e) {
+    const catContainer = e.target.closest(".cat-container");
+    if (!catContainer) return;
+
+    const catId = catContainer.dataset.catId;
+    catContainer.remove();
+    const catCheckbox = Array.from(
+      catDropdown.querySelectorAll('li input[type="checkbox"]')
+    ).find((el) => el.value === catId);
+    catCheckbox.checked = false;
+
+    if (
+      Array.from(catDropdown.querySelectorAll('li input[type="checkbox"]'))
+        .map((el) => el.checked)
+        .filter((v) => v).length === 0
+    ) {
+      catRenderer.style.display = "none";
+    }
+  }
+
+  catRenderer.addEventListener("click", handleRendererClick);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   initDarkMode();
   initSearchDropdown();
+  initCategoriesSelector();
 });

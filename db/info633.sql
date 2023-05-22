54a3e08aacd07a7dab8d228b5a59b8bf6f35006e
loisblin <113303000+loisblin@users.noreply.github.com>
2023-05-22 12:32:37 +0200

    [FIX]: base de donnée ajout id user dans les question

--- db/info633.sql
+++ db/info633.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : dim. 21 mai 2023 à 16:15
+-- Généré le : lun. 22 mai 2023 à 10:13
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -157,18 +157,18 @@ CREATE TABLE IF NOT EXISTS `question` (
 --
 
 INSERT INTO `question` (`id`, `title`, `creation_date`, `number_likes`, `content`, `id_user`) VALUES
-(1, 'Connexion à la base de donnée en PHP', '2023-02-02 00:00:00', 0, 'J\'ai une erreur quand j\'essaye de connecter mon site à ma base de donnée mysql, j\'obtiens l\'error \"Vous n\'êtes pas qualifié pour parler de mon grand front\".\r\nVoici mon code :\r\n<?php\r\n$serveur = \"localhost\";\r\n$utilisateur = \"jeanlasticot\";\r\n$motdepasse = \"jesuisunasticot joyeux\";\r\n$base_de_donnees =\"aaaaaaugh\";\r\n\r\n$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $base_de_donnees);\r\n\r\nif (!$connexion) {\r\n    die(\"Erreur de connexion à la base de données : \" . mysqli_connect_error());\r\n}', NULL),
-(2, 'Transformation d’une association de 1 à 1', '2023-01-29 00:00:00', 4, 'Bonjour, nous avons vu comment passer du schéma entités/associations au schéma relationnel pour des associations de 1 à plusieurs et de plusieurs à plusieurs. Mais pour une association de 1 à 1 comment cela se passe ? On ajoute n\'importe laquelle clé primaire d\'une entité dans la relation de l\'autre entité en tant que clé étrangère ?\r\n', NULL),
-(3, 'Nommage des tables', '2023-01-30 00:00:00', 12, 'Est ce qu\'il existe des conventions de nommages des tables / attributs dans le milieu professionnel de la base de donnés ?', NULL),
-(4, 'Table et relation', '2023-01-30 00:00:00', 1, 'Bonjour, je me demandais quelle est la différence entre une table et une relation ?', NULL),
-(5, 'Nombre de relation', '2023-01-30 00:00:00', 5, 'est-il possible d\'avoir deux ou plusieurs relations entre deux mêmes entités ?', NULL),
-(6, 'tuple et n-tuple', '2023-01-31 00:00:00', 11, 'Que signifie \"tuple\" et \"n-uplet\" d\'un modèle ?', NULL),
-(7, 'Merise et modèle entité association', '2023-01-01 00:00:00', 0, 'Quelles est la différence entre le modèle E/A et le modèle Merise ?\r\n', NULL),
-(8, 'Groupe by et Order by', '2023-02-14 00:00:00', 27, 'Quelle est la différence entre GROUP BY et ORDER BY ?', NULL),
-(9, 'Join et select imbriqué', '2023-02-22 00:00:00', 5, 'Bonjour, \r\n\r\nJe me demandais dans quelle situation il est préférable d\'utiliser un SELECT imbriquée et dans quelle situation il est préférable d\'utiliser un JOIN ?\r\n\r\nCordialement,\r\n\r\nUgo TAFARO', NULL),
-(10, 'Left et right join en SQL', '2023-02-22 00:00:00', 12, 'Bonjour, \r\n\r\nJe souhaiterais savoir à quoi servent les left et right join puisqu\'ils sélectionnent l\'intégralité d\'une seule table (comme un SELECT * FROM table).\r\n\r\nCordialement', NULL),
-(11, 'Jointure et produit cartésien', '2023-02-22 00:00:00', 1, 'Bonjour,\r\n\r\nj\'aimerais connaître la différence entre une jointure et un produit cartésien\r\n\r\nmerci,', NULL),
-(12, 'Usage des vues', '2023-02-27 00:00:00', 5, 'Quels sont les désavantages d\'utiliser une vue au lieu d\'une table ?', NULL);
+(1, 'Connexion à la base de donnée en PHP', '2023-02-02 00:00:00', 0, 'J\'ai une erreur quand j\'essaye de connecter mon site à ma base de donnée mysql, j\'obtiens l\'error \"Vous n\'êtes pas qualifié pour parler de mon grand front\".\r\nVoici mon code :\r\n<?php\r\n$serveur = \"localhost\";\r\n$utilisateur = \"jeanlasticot\";\r\n$motdepasse = \"jesuisunasticot joyeux\";\r\n$base_de_donnees =\"aaaaaaugh\";\r\n\r\n$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $base_de_donnees);\r\n\r\nif (!$connexion) {\r\n    die(\"Erreur de connexion à la base de données : \" . mysqli_connect_error());\r\n}', 2),
+(2, 'Transformation d’une association de 1 à 1', '2023-01-29 00:00:00', 4, 'Bonjour, nous avons vu comment passer du schéma entités/associations au schéma relationnel pour des associations de 1 à plusieurs et de plusieurs à plusieurs. Mais pour une association de 1 à 1 comment cela se passe ? On ajoute n\'importe laquelle clé primaire d\'une entité dans la relation de l\'autre entité en tant que clé étrangère ?\r\n', 1),
+(3, 'Nommage des tables', '2023-01-30 00:00:00', 12, 'Est ce qu\'il existe des conventions de nommages des tables / attributs dans le milieu professionnel de la base de donnés ?', 5),
+(4, 'Table et relation', '2023-01-30 00:00:00', 1, 'Bonjour, je me demandais quelle est la différence entre une table et une relation ?', 3),
+(5, 'Nombre de relation', '2023-01-30 00:00:00', 5, 'est-il possible d\'avoir deux ou plusieurs relations entre deux mêmes entités ?', 6),
+(6, 'tuple et n-tuple', '2023-01-31 00:00:00', 11, 'Que signifie \"tuple\" et \"n-uplet\" d\'un modèle ?', 7),
+(7, 'Merise et modèle entité association', '2023-01-01 00:00:00', 0, 'Quelles est la différence entre le modèle E/A et le modèle Merise ?\r\n', 4),
+(8, 'Groupe by et Order by', '2023-02-14 00:00:00', 27, 'Quelle est la différence entre GROUP BY et ORDER BY ?', 1),
+(9, 'Join et select imbriqué', '2023-02-22 00:00:00', 5, 'Bonjour, \r\n\r\nJe me demandais dans quelle situation il est préférable d\'utiliser un SELECT imbriquée et dans quelle situation il est préférable d\'utiliser un JOIN ?\r\n\r\nCordialement,\r\n\r\nUgo TAFARO', 3),
+(10, 'Left et right join en SQL', '2023-02-22 00:00:00', 12, 'Bonjour, \r\n\r\nJe souhaiterais savoir à quoi servent les left et right join puisqu\'ils sélectionnent l\'intégralité d\'une seule table (comme un SELECT * FROM table).\r\n\r\nCordialement', 6),
+(11, 'Jointure et produit cartésien', '2023-02-22 00:00:00', 1, 'Bonjour,\r\n\r\nj\'aimerais connaître la différence entre une jointure et un produit cartésien\r\n\r\nmerci,', 5),
+(12, 'Usage des vues', '2023-02-27 00:00:00', 5, 'Quels sont les désavantages d\'utiliser une vue au lieu d\'une table ?', 2);
 
 -- --------------------------------------------------------
 
@@ -183,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_name` varchar(25) NOT NULL,
   `is_admin` tinyint(1) NOT NULL,
   `user_name` varchar(25) NOT NULL,
-  `image_url` text NOT NULL,
+  `profile_url` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `password` varchar(256) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -194,16 +194,16 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `first_name`, `last_name`, `is_admin`, `user_name`, `image_url`, `password`) VALUES
-(1, 'hugo', 'beaubrun', 0, 'beaubruh', '', '63a9f0ea7bb98050796b649e85481845'),
-(2, 'ronan', 'terras', 0, 'ronang', '', '63a9f0ea7bb98050796b649e85481845'),
-(3, 'carlyne', 'barrachin', 0, 'carlyne', '', '63a9f0ea7bb98050796b649e85481845'),
-(4, 'lois', 'blin', 0, 'lois', '', '63a9f0ea7bb98050796b649e85481845'),
-(5, 'alexis', 'thierry', 0, 'alexis', '', '63a9f0ea7bb98050796b649e85481845'),
-(6, 'arnaud', 'pfundstein', 0, 'arnaud', '', '63a9f0ea7bb98050796b649e85481845'),
-(7, 'ugo', 'tafaro', 0, 'ugo', '', '63a9f0ea7bb98050796b649e85481845'),
-(8, 'lionel', 'valet', 1, 'lionel', '', '63a9f0ea7bb98050796b649e85481845'),
-(9, 'ilham', 'alloui', 1, 'ilham', '', '63a9f0ea7bb98050796b649e85481845');
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `is_admin`, `user_name`, `profile_url`, `password`, `email`) VALUES
+(1, 'hugo', 'beaubrun', 0, 'beaubruh', '', '63a9f0ea7bb98050796b649e85481845', NULL),
+(2, 'ronan', 'terras', 0, 'ronang', '', '63a9f0ea7bb98050796b649e85481845', NULL),
+(3, 'carlyne', 'barrachin', 0, 'carlyne', '', '63a9f0ea7bb98050796b649e85481845', NULL),
+(4, 'Lois', 'Blin', 0, 'lois', '', '389a20c65b0987b03475f7d79c3ff974', 'blinlois@gmail.com'),
+(5, 'alexis', 'thierry', 0, 'alexis', '', '63a9f0ea7bb98050796b649e85481845', NULL),
+(6, 'arnaud', 'pfundstein', 0, 'arnaud', '', '63a9f0ea7bb98050796b649e85481845', NULL),
+(7, 'ugo', 'tafaro', 0, 'ugo', '', '63a9f0ea7bb98050796b649e85481845', 'utafaro@gmail.com'),
+(8, 'lionel', 'valet', 1, 'lionel', '', '63a9f0ea7bb98050796b649e85481845', NULL),
+(9, 'ilham', 'alloui', 1, 'ilham', '', '63a9f0ea7bb98050796b649e85481845', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

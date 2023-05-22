bdec35b084d735ca95584b7040841cdea90eb8fe
Arnaud Pfundstein <arnaudpfundstein.pro@gmail.com>
2023-05-22 13:41:44 +0200

    Feature: create question.php and answer.php includes

--- single-question.php
+++ single-question.php
@@ -7,16 +7,15 @@ $user = get_user();
 ?>
 
 <section class="flex pt-4 pb-8 lg:pt-8 lg:pb-12 bg-white dark:bg-gray-900">
-	<div class="flex justify-between px-4 mx-auto max-w-screen-md flex-1">
+	<div class="flex flex-col justify-between px-4 mx-auto max-w-screen-md flex-1">
 
 		<?php
 
 		if ( key_exists( 'new_question', $_GET ) ) {
 			require_once 'includes/new-question-form.php';
 		} elseif ( key_exists( 'id', $_GET ) ) {
-			$id       = htmlentities( $_GET['id'] );
-			$question = Database::get_question_by_id( $id );
-			$answers  = Database::get_answer_by_question_id( $id );
+			require_once 'includes/question.php';
+			require_once 'includes/answer.php';
 		} else {
 			AlertManager::display_info( 'Aucune question n\'a été sélectionnée' );
 		}

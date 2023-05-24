83c1a27b42360ee9f08700cfe83dce1d41253cd6
loisblin <113303000+loisblin@users.noreply.github.com>
2023-05-24 14:00:54 +0200

    [FIX] like bdd

--- db/info633.sql
+++ db/info633.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 24 mai 2023 à 10:57
+-- Généré le : mer. 24 mai 2023 à 11:59
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -137,6 +137,65 @@ INSERT INTO `is_nearby` (`id_question1`, `id_question2`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_question` int DEFAULT NULL,
+  `id_user` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_question` (`id_question`),
+  KEY `fk_id_user` (`id_user`)
+) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `likes`
+--
+
+INSERT INTO `likes` (`id`, `id_question`, `id_user`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 4),
+(4, 2, 1),
+(5, 2, 3),
+(6, 2, 4),
+(7, 2, 7),
+(8, 3, 6),
+(9, 3, 2),
+(10, 4, 1),
+(11, 4, 3),
+(12, 4, 2),
+(13, 5, 1),
+(14, 5, 2),
+(15, 5, 3),
+(16, 6, 4),
+(17, 6, 5),
+(18, 6, 6),
+(19, 7, 7),
+(20, 7, 1),
+(21, 7, 2),
+(22, 8, 3),
+(23, 8, 4),
+(24, 8, 5),
+(25, 9, 6),
+(26, 9, 7),
+(27, 10, 1),
+(28, 10, 2),
+(29, 10, 3),
+(30, 11, 2),
+(31, 11, 3),
+(32, 11, 1),
+(33, 11, 6),
+(34, 12, 5),
+(35, 12, 3),
+(36, 12, 7),
+(37, 12, 4);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `question`
 --
 
@@ -145,7 +204,6 @@ CREATE TABLE IF NOT EXISTS `question` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `number_likes` int NOT NULL DEFAULT '0',
   `content` text NOT NULL,
   `id_user` int DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -156,19 +214,19 @@ CREATE TABLE IF NOT EXISTS `question` (
 -- Déchargement des données de la table `question`
 --
 
-INSERT INTO `question` (`id`, `title`, `creation_date`, `number_likes`, `content`, `id_user`) VALUES
-(1, 'Connexion à la base de donnée en PHP', '2023-02-02 00:00:00', 0, 'J\'ai une erreur quand j\'essaye de connecter mon site à ma base de donnée mysql, j\'obtiens l\'error \"Vous n\'êtes pas qualifié pour parler de mon grand front\".\r\nVoici mon code :\r\n<?php\r\n$serveur = \"localhost\";\r\n$utilisateur = \"jeanlasticot\";\r\n$motdepasse = \"jesuisunasticot joyeux\";\r\n$base_de_donnees =\"aaaaaaugh\";\r\n\r\n$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $base_de_donnees);\r\n\r\nif (!$connexion) {\r\n    die(\"Erreur de connexion à la base de données : \" . mysqli_connect_error());\r\n}', 2),
-(2, 'Transformation d’une association de 1 à 1', '2023-01-29 00:00:00', 4, 'Bonjour, nous avons vu comment passer du schéma entités/associations au schéma relationnel pour des associations de 1 à plusieurs et de plusieurs à plusieurs. Mais pour une association de 1 à 1 comment cela se passe ? On ajoute n\'importe laquelle clé primaire d\'une entité dans la relation de l\'autre entité en tant que clé étrangère ?\r\n', 1),
-(3, 'Nommage des tables', '2023-01-30 00:00:00', 12, 'Est ce qu\'il existe des conventions de nommages des tables / attributs dans le milieu professionnel de la base de donnés ?', 5),
-(4, 'Table et relation', '2023-01-30 00:00:00', 1, 'Bonjour, je me demandais quelle est la différence entre une table et une relation ?', 3),
-(5, 'Nombre de relation', '2023-01-30 00:00:00', 5, 'est-il possible d\'avoir deux ou plusieurs relations entre deux mêmes entités ?', 6),
-(6, 'tuple et n-tuple', '2023-01-31 00:00:00', 11, 'Que signifie \"tuple\" et \"n-uplet\" d\'un modèle ?', 7),
-(7, 'Merise et modèle entité association', '2023-01-01 00:00:00', 0, 'Quelles est la différence entre le modèle E/A et le modèle Merise ?\r\n', 4),
-(8, 'Groupe by et Order by', '2023-02-14 00:00:00', 27, 'Quelle est la différence entre GROUP BY et ORDER BY ?', 1),
-(9, 'Join et select imbriqué', '2023-02-22 00:00:00', 5, 'Bonjour, \r\n\r\nJe me demandais dans quelle situation il est préférable d\'utiliser un SELECT imbriquée et dans quelle situation il est préférable d\'utiliser un JOIN ?\r\n\r\nCordialement,\r\n\r\nUgo TAFARO', 3),
-(10, 'Left et right join en SQL', '2023-02-22 00:00:00', 12, 'Bonjour, \r\n\r\nJe souhaiterais savoir à quoi servent les left et right join puisqu\'ils sélectionnent l\'intégralité d\'une seule table (comme un SELECT * FROM table).\r\n\r\nCordialement', 6),
-(11, 'Jointure et produit cartésien', '2023-02-22 00:00:00', 1, 'Bonjour,\r\n\r\nj\'aimerais connaître la différence entre une jointure et un produit cartésien\r\n\r\nmerci,', 5),
-(12, 'Usage des vues', '2023-02-27 00:00:00', 5, 'Quels sont les désavantages d\'utiliser une vue au lieu d\'une table ?', 2);
+INSERT INTO `question` (`id`, `title`, `creation_date`, `content`, `id_user`) VALUES
+(1, 'Connexion à la base de donnée en PHP', '2023-02-02 00:00:00', 'J\'ai une erreur quand j\'essaye de connecter mon site à ma base de donnée mysql, j\'obtiens l\'error \"Vous n\'êtes pas qualifié pour parler de mon grand front\".\r\nVoici mon code :\r\n<?php\r\n$serveur = \"localhost\";\r\n$utilisateur = \"jeanlasticot\";\r\n$motdepasse = \"jesuisunasticot joyeux\";\r\n$base_de_donnees =\"aaaaaaugh\";\r\n\r\n$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse, $base_de_donnees);\r\n\r\nif (!$connexion) {\r\n    die(\"Erreur de connexion à la base de données : \" . mysqli_connect_error());\r\n}', 2),
+(2, 'Transformation d’une association de 1 à 1', '2023-01-29 00:00:00', 'Bonjour, nous avons vu comment passer du schéma entités/associations au schéma relationnel pour des associations de 1 à plusieurs et de plusieurs à plusieurs. Mais pour une association de 1 à 1 comment cela se passe ? On ajoute n\'importe laquelle clé primaire d\'une entité dans la relation de l\'autre entité en tant que clé étrangère ?\r\n', 1),
+(3, 'Nommage des tables', '2023-01-30 00:00:00', 'Est ce qu\'il existe des conventions de nommages des tables / attributs dans le milieu professionnel de la base de donnés ?', 5),
+(4, 'Table et relation', '2023-01-30 00:00:00', 'Bonjour, je me demandais quelle est la différence entre une table et une relation ?', 3),
+(5, 'Nombre de relation', '2023-01-30 00:00:00', 'est-il possible d\'avoir deux ou plusieurs relations entre deux mêmes entités ?', 6),
+(6, 'tuple et n-tuple', '2023-01-31 00:00:00', 'Que signifie \"tuple\" et \"n-uplet\" d\'un modèle ?', 7),
+(7, 'Merise et modèle entité association', '2023-01-01 00:00:00', 'Quelles est la différence entre le modèle E/A et le modèle Merise ?\r\n', 4),
+(8, 'Groupe by et Order by', '2023-02-14 00:00:00', 'Quelle est la différence entre GROUP BY et ORDER BY ?', 1),
+(9, 'Join et select imbriqué', '2023-02-22 00:00:00', 'Bonjour, \r\n\r\nJe me demandais dans quelle situation il est préférable d\'utiliser un SELECT imbriquée et dans quelle situation il est préférable d\'utiliser un JOIN ?\r\n\r\nCordialement,\r\n\r\nUgo TAFARO', 3),
+(10, 'Left et right join en SQL', '2023-02-22 00:00:00', 'Bonjour, \r\n\r\nJe souhaiterais savoir à quoi servent les left et right join puisqu\'ils sélectionnent l\'intégralité d\'une seule table (comme un SELECT * FROM table).\r\n\r\nCordialement', 6),
+(11, 'Jointure et produit cartésien', '2023-02-22 00:00:00', 'Bonjour,\r\n\r\nj\'aimerais connaître la différence entre une jointure et un produit cartésien\r\n\r\nmerci,', 5),
+(12, 'Usage des vues', '2023-02-27 00:00:00', 'Quels sont les désavantages d\'utiliser une vue au lieu d\'une table ?', 2);
 
 -- --------------------------------------------------------
 
@@ -198,7 +256,7 @@ INSERT INTO `user` (`id`, `first_name`, `last_name`, `is_admin`, `user_name`, `p
 (1, 'hugo', 'beaubrun', 0, 'beaubruh', '', '63a9f0ea7bb98050796b649e85481845', NULL),
 (2, 'ronan', 'terras', 0, 'ronang', '', '63a9f0ea7bb98050796b649e85481845', NULL),
 (3, 'carlyne', 'barrachin', 0, 'carlyne', '', '63a9f0ea7bb98050796b649e85481845', NULL),
-(4, 'Lois', 'Blin', 0, 'lois', '', '389a20c65b0987b03475f7d79c3ff974', 'blinlois@gmail.com'),
+(4, 'Lois', 'Blin', 0, 'lois', 'https://img.freepik.com/vecteurs-premium/profil-avatar-illustration-coloree-2_549209-82.jpg?w=2000', '389a20c65b0987b03475f7d79c3ff974', 'blinlois@gmail.com'),
 (5, 'alexis', 'thierry', 0, 'alexis', '', '63a9f0ea7bb98050796b649e85481845', NULL),
 (6, 'arnaud', 'pfundstein', 0, 'arnaud', '', '63a9f0ea7bb98050796b649e85481845', NULL),
 (7, 'ugo', 'tafaro', 0, 'ugo', '', '63a9f0ea7bb98050796b649e85481845', 'utafaro@gmail.com'),

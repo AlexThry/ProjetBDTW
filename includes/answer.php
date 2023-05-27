c9b53712739fd2c284d347e808eec8e3eafa9214
Alexis Thierry <alexist103@gmail.com>
2023-05-27 22:36:49 +0200

    FIX : Verif existance de question lors de l'affichage

--- includes/answer.php
+++ includes/answer.php
@@ -10,8 +10,15 @@ if ( ! key_exists( 'id', $_GET ) ) {
 $current_user = get_user();
 
 $id     = (int)htmlentities( $_GET['id'] );
+if (Database::get_question_answer( $id )) {
+	$is_answered = true;
+	$answer_id = Database::get_question_answer( $id )['id'];
+	$answer = Database::get_question_answer( $id );
+	$user = Database::get_user( $answer['id_user'] );
+} else {
+	$is_answered = false;
+}
 
-$answer_id = Database::get_question_answer( $id )['id'];
 
 if (key_exists('answer-content', $_POST) && isset($_POST['answer-content'])) {
 	Database::modify_answer($answer_id, htmlentities($_POST['answer-content']));
@@ -21,8 +28,6 @@ if (key_exists('answer-content', $_POST) && isset($_POST['answer-content'])) {
 
 
 
-$answer = Database::get_question_answer( $id );
-$user = Database::get_user( $answer['id_user'] );
 
 
 // todo : check if user is admin
@@ -30,87 +35,96 @@ $is_admin = false;
 if ($current_user['is_admin']) {
 	$is_admin = true;
 }
+if ($is_answered) {
 
-
-if ($user['profile_url']) {
-	$admin_image = $user['profile_url'];
-} else {
-	$admin_image = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
-}
-// todo: recup user et afficher son nom, prenom, image url si oui sinon mettre une image par defaut
-
-if ( key_exists( 'edit_answer', $_GET ) ) {
-	if ( $is_admin ) { 
-		?>
-		<form action="single-question.php?id=<?php echo $id ?>" method="post">
-			<textarea id="answer-editor" required="" name="answer-content" rows="8" class="block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-				<?php echo $answer['content'] ?>
-			</textarea>
-			<input type="submit" value="valider" class="rounded-md bg-indigo-600 mb-4 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
-		</form>
-
-		<?php
-		
+	if ($is_answered && $user['profile_url']) {
+		$admin_image = $user['profile_url'];
 	} else {
-		AlertManager::display_warning( "T'es un petit malin toi ;). Désolé tu n'as pas le droit de modifier cette question." );
+		$admin_image = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
+	}
+	// todo: recup user et afficher son nom, prenom, image url si oui sinon mettre une image par defaut
+	
+	if ( key_exists( 'edit_answer', $_GET ) ) {
+		if ( $is_admin ) { 
+			?>
+			<form action="single-question.php?id=<?php echo $id ?>" method="post">
+			<?php if ($is_answered) : ?>
+				<textarea id="answer-editor" required="" name="answer-content" rows="8" class="block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<?php echo $answer['content'] ?>
+				</textarea>
+				<input type="submit" value="valider" class="rounded-md bg-indigo-600 mb-4 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
+			<?php else : ?>
+				<textarea id="answer-editor" required="" name="answer-content" rows="8" class="block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					Pas de réponse pour le moment
+				</textarea>
+			</form>
+	
+			<?php endif; ?>
+	
+			<?php
+			
+		} else {
+			AlertManager::display_warning( "T'es un petit malin toi ;). Désolé tu n'as pas le droit de modifier cette question." );
+		}
+	} else {
+		?>
+		<article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900 border border-gray-200 rounded-lg dark:border-gray-700">
+			<footer class="flex justify-between items-center mb-2">
+				<div class="flex items-center">
+					<p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
+							class="mr-2 w-6 h-6 rounded-full"
+							src="<?php echo $admin_image ?>"
+							alt="Michael Gough"><?php echo $user['first_name'] . " " . $user['last_name'] ?></p>
+					<?php if ( $is_answered ) : ?>
+				
+					<?php endif; ?>
+				</div>
+	
+				<?php if ( $is_admin ) : ?>
+					<button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
+						class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+						type="button">
+						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
+							</path>
+						</svg>
+						<span class="sr-only">Options</span>
+					</button>
+					<!-- Dropdown menu -->
+					<div id="dropdownComment1"
+						class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+						<ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+							aria-labelledby="dropdownMenuIconHorizontalButton">
+							<li>
+								<a href="?id=<?php echo htmlentities( $id ); ?>&edit_answer=true"
+									class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+							</li>
+						</ul>
+					</div>
+				<?php endif; ?>
+			</footer>
+			<mark class="text-gray-800 dark:text-gray-200 html-markdown-renderer">
+				<?php 
+					if ( $is_answered ) {
+						echo html_entity_decode( $answer['content'] ); 
+					}
+				?>
+			</mark>
+	
+			<!-- Pour aller plus loin. Ajouter bouton répondre -->
+			<!-- <div class="flex items-center mt-4 space-x-4">
+				<button type="button"
+					class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400">
+					<svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
+					Répondre
+				</button>
+			</div> -->
+		</article>
+		<?php
 	}
 } else {
-	?>
-	<article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900 border border-gray-200 rounded-lg dark:border-gray-700">
-		<footer class="flex justify-between items-center mb-2">
-			<div class="flex items-center">
-				<p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
-						class="mr-2 w-6 h-6 rounded-full"
-						src="<?php echo $admin_image ?>"
-						alt="Michael Gough"><?php echo $user['first_name'] . " " . $user['last_name'] ?></p>
-				<?php if ( $answer != null && key_exists( 'creation_date', $answer ) ) : ?>
-					<p class="text-sm text-gray-600 dark:text-gray-400">
-						<time class="inline" pubdate datetime="<?php echo htmlentities( $answer['creation_date'] ); ?>" title="<?php echo format_date( $answer['creation_date'] ); ?>"><?php echo format_date( $answer['creation_date'] ); ?></time>
-					</p>
-				<?php endif; ?>
-			</div>
-
-			<?php if ( $is_admin ) : ?>
-				<button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
-					class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-					type="button">
-					<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg">
-						<path
-							d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
-						</path>
-					</svg>
-					<span class="sr-only">Options</span>
-				</button>
-				<!-- Dropdown menu -->
-				<div id="dropdownComment1"
-					class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-					<ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
-						aria-labelledby="dropdownMenuIconHorizontalButton">
-						<li>
-							<a href="?id=<?php echo htmlentities( $id ); ?>&edit_answer=true"
-								class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-						</li>
-					</ul>
-				</div>
-			<?php endif; ?>
-		</footer>
-		<mark class="text-gray-800 dark:text-gray-200 html-markdown-renderer">
-			<?php 
-				if ($answer != null ) {
-					echo html_entity_decode( $answer['content'] ); 
-				}
-			?>
-		</mark>
-
-		<!-- Pour aller plus loin. Ajouter bouton répondre -->
-		<!-- <div class="flex items-center mt-4 space-x-4">
-			<button type="button"
-				class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400">
-				<svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
-				Répondre
-			</button>
-		</div> -->
-	</article>
-	<?php
+	AlertManager::display_warning( "Cette question n'a pas encore de réponse" );
 }
+

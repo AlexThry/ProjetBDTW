ce59ce67d843909a43b3311e99d160e72386934b
Arnaud Pfundstein <arnaudpfundstein.pro@gmail.com>
2023-05-31 16:02:17 +0200

    Feature: add hero image

--- index.php
+++ index.php
@@ -12,17 +12,26 @@ $is_search      = $category_label !== null || $search_string !== null;
         <?php require_once 'includes/admin-dashboard.php' ?>
     <?php else : ?>
         <section class="relative bg-white dark:bg-gray-800">
-            <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+            <div class="pb-80 pt-4 sm:pb-40 sm:pt-6 lg:pb-48 lg:pt-10">
                 <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
                     <?php if ( !$is_search ) : ?>
-                        <div class="sm:max-w-lg">
-                            <h1 class="font text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">Ne laissez aucune question sans réponse</h1>
-                            <p class="mt-4 text-xl text-gray-500 dark:text-gray-300">Trouvez des solutions et approfondissez vos connaissances en posant toutes vos questions sur le cours sur notre site interactif et bénéficiez des réponses de nos experts!</p>
+
+                        <div class="grid px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-2">
+                            <div class="sm:max-w-lg mt-10">
+                                <h1 class="font text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">Ne laissez aucune question sans réponse</h1>
+                                <p class="mt-4 text-xl text-gray-500 dark:text-gray-300">Trouvez des solutions et approfondissez vos connaissances en posant toutes vos questions sur le cours sur notre site interactif et bénéficiez des réponses de nos experts!</p>
+                                <hr class="mt-10 opacity-0">
+                                <?php require 'includes/search-bar.php'; ?>
+                            </div>
+                            <div class="w-full flex">
+                                <img class="w-full flex-1 dark-theme h-8 w-auto" style="height:500px;" src="./assets/images/programming-dark.svg" alt="Trouvez des solutions et approfondissez vos connaissances en posant toutes vos questions">
+                                <img class="w-full flex-1 light-theme h-8 w-auto" style="height:500px;" src="./assets/images/programming-light.svg" alt="Trouvez des solutions et approfondissez vos connaissances en posant toutes vos questions">
+                            </div>
                         </div>
+                        
                     <?php endif; ?>
                     <div class="mt-10">
 
-                        <?php require 'includes/search-bar.php'; ?>
 
                         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
                             <div class="mt-10 bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">

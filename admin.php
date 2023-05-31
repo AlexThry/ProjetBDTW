a07ef7a61a274e251a5332f6ce76f6923e6967a5
hug0be <83722580+hug0be@users.noreply.github.com>
2023-05-31 20:00:45 +0200

    [CLEAN] Refactor login.php

--- admin.php
+++ admin.php
@@ -14,7 +14,7 @@ require_once 'includes/header.php';
 			<div class="sm:mx-auto sm:w-full sm:max-w-sm">
 				<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">Admin</h2>
 			</div>
-	
+
 			<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 				<form class="space-y-6" action="login.php" method="POST">
 					<?php
@@ -22,14 +22,14 @@ require_once 'includes/header.php';
 						AlertManager::display_error( html_entity_decode( $_GET['connection_error'] ) );
 					}
 					?>
-	
+
 				<div>
-					<label for="connection-user_name" class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Nom d'utilisateur</label>
+					<label for="connection-user-name" class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Nom d'utilisateur</label>
 					<div class="mt-2">
-					<input  <?php display_input_value( 'user_name' ); ?>  type="text" name="connection-user_name" id="connection-user_name-input-creation" placeholder="Saisissez un nom d'utilisateur" required autocomplete="off" required class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<input  <?php display_input_value( 'user_name' ); ?>  type="text" name="connection-user-name" id="connection-user-name-input-creation" placeholder="Saisissez un nom d'utilisateur" required autocomplete="off" required class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 					</div>
 				</div>
-	
+
 				<div>
 					<div class="flex items-center justify-between">
 					<label for="connection-password" class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Mot de passe</label>
@@ -38,12 +38,12 @@ require_once 'includes/header.php';
 					<input type="password" name="connection-password" id="connection-password-input-creation" placeholder="Saisissez un mot de passe" autocomplete="current-connection-password" required class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 					</div>
 				</div>
-	
+
 				<div>
 					<button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Se connecter</button>
 				</div>
 				</form>
-	
+
 			</div>
 		</div>
         <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/authentication/illustration.svg" alt="">
--- login.php
+++ login.php
@@ -5,17 +5,15 @@
 
 require_once 'functions.php';
 
-if ( isset( $_POST['connection-user_name'] ) && isset( $_POST['connection-password'] ) ) {
-	$res = connect_user( htmlentities( $_POST['connection-user_name'] ), $_POST['connection-password'] );
+$user_name = isset($_POST['connection-user-name']) ? htmlentities($_POST['connection-user-name']) : null;
+$password  = isset($_POST['connection-password']) ? htmlentities($_POST['connection-password']) : null;
 
-	// This is painful, but we must keep the "=== true", because connect_user returns a bool or a non-empty string
-	if ( $res === true ) {
-		header( 'Location: index.php' );
-	} else {
-		header( 'Location: connection.php?connection_error=' . htmlentities( $res ) );
-	}
-	exit();
+if ( $user_name === null || $password === null ) {
+	header("Location: connection.php");
 }
 
-header( 'Location: connection.php' );
-exit();
+$res = connect_user( $user_name, $password );
+
+// This is painful, but we must keep the "=== true", because connect_user returns a bool or a non-empty string
+header($res === true ? "Location: index.php" : "Location: connection.php?connection_error=$res");
+exit;
